0da5585e1bf2f7bcc99f979cf2917e888431ad1e
shenglu <shenglu@shengludemacbook-pro.local>
2016-02-27 14:05:25 +0800

    Initial Commit

---
+++ ipadsanxia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ipadsanxia
+//
+//  Created by shenglu on 16/2/27.
+//  Copyright © 2016年 shenglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
